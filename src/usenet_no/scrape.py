db94d49846f14a40ae88fdb057a14824b65229b4
Tita <tita.enstad@nb.no>
2024-11-25 15:11:22 +0100

    Scrape usenet.no

---
+++ src/usenet_no/scrape.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+from tqdm import tqdm
+
+
+def get_page_data(page_url: str, page_data_file: Path, download_again: bool) -> str:
+    if not page_data_file.exists() or download_again:
+        page = requests.get(page_url)
+        if page.ok:
+            with page_data_file.open("w+") as f:
+                f.write(page.text)
+    return page_data_file.read_text()
+
+
+def get_urls(html: str) -> set[str]:
+    soup = BeautifulSoup(html, "html.parser")
+
+    urls = set()
+    for e in soup.find_all(
+        "a", href=lambda href: href and "no" in href and ".zip" in href
+    ):
+        urls.add(e.get("href"))
+    return urls
+
+
+def download_zip(url: str, local_filename: Path):
+    # Send a GET request to the URL. Use stream=True to enable streaming mode.
+    with requests.get(url, stream=True) as response:
+        # Check if the request was successful
+        response.raise_for_status()
+
+        # Open the local file in binary write mode
+        with open(local_filename, "wb") as file:
+            # Iterate over the response in chunks
+            for chunk in response.iter_content(chunk_size=8192):
+                # Write each chunk to the file
+                file.write(chunk)
+
+
+if __name__ == "__main__":
+    page_data = Path("page_data.txt")
+    base_url = "https://archive.org/download/usenet-no"
+    data_dir = Path("data")
+
+    if not data_dir.exists():
+        data_dir.mkdir()
+
+    page_data = get_page_data(
+        page_url=base_url, page_data_file=page_data, download_again=False
+    )
+    urls = get_urls(page_data)
+
+    for url in tqdm(urls):
+        if url.endswith("/"):
+            url = url[:-1]
+        zip_url = base_url + "/" + url
+        local_file = data_dir / url
+        if not local_file.exists():
+            download_zip(zip_url, local_filename=local_file)
